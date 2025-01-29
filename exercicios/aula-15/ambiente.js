d5087658300998d9524fcc4bd5da0a1e5f2e576b
Márcio Miranda <147844747+marciomrd@users.noreply.github.com>
2025-01-29 12:55:34 -0300

    Create aula-15
    Create aula-15 for test js arrays

---
+++ exercicios/aula-15/ambiente.js
@@ -0,0 +1,15 @@
+let num = [5,8,2,9,3]
+
+/*num.push(1)
+
+num.sort()
+
+console.log(num)
+console.log(`Nosso vetor tem ${num.length} posições`)*/
+
+/*for(let pos in num){
+    console.log(`Aminha posição ${pos} tem o valor ${num[pos]}`)
+}*/
+
+let pos = num.indexOf(9)
+console.log(`A posição ${pos} contém o valor 9`)
